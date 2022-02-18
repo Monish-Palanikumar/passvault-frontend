cfacd2462f7a0c69871ef29848bdde13959394cb
Monish-Palanikumar <monishpalanikumar@gmail.com>
2022-02-18 21:16:41 +0530

    login form partial functionality

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,25 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
+     const [username, setUsername] = useState("");
+     const [password, setPassword] = useState("");
+     const handleSubmit = (e) => {
+          e.preventDefault();
+          if (username === '' || password === '') {
+               alert("Missing username/password");
+          }
+          else {
+               // both values entered
+          }
+     }
      return (
           <div id="sect login-section" className='container'>
                <h1 className='text-center title'>Login Now !!</h1>
                <form className="login-form grid" method="post">
                     <div className="login-inputs grid">
                          <div className='login-content'>
-                              <label htmlFor='' className='login-label'>Username</label>
-                              <input type="text" className="login-input" />
+                              <label htmlFor='username' className='login-label'>Username</label>
+                              <input id="username" type="text" className="login-input" value={username} onChange={(e) => setUsername(e.target.value)} />
                          </div>
                          <div className='login-content'>
-                              <label htmlFor='' className='login-label'>Master Password</label>
-                              <input type="password" className="login-input" />
+                              <label htmlFor='password' className='login-label'>Master Password</label>
+                              <input id="password" type="password" className="login-input" value={password} onChange={(e) => setPassword(e.target.value)} />
                          </div>
                     </div>
                     <div>
-                         {/* TODO Insert link */}
-                         <a href="http://localhost:3000/dashboard" className="button button-full button--flex">
-                              Get Access !
-                         </a>
+                         <button className='button button-full button--flex' onClick={handleSubmit}>Get Access !</button>
                     </div>
                </form>
           </div>
